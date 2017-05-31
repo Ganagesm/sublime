09e88ce418278f04f16ada63e0419b9198f328fb
Sopan Ganage <gangesm1@gmail.com>
2017-05-31 17:29:24 +0530

    Create Header.php

---
+++ Header.php
@@ -0,0 +1,5 @@
+<li><a href="index.php" <?php if($page_name=="index.php"){?> class="active"<?php }?>>Home</a></li>
+<li><a href="#" <?php if($page_name=="about.php"){?> class="active"<?php }?>>About</a></li>
+<li><a href="#" <?php if($page_name=="blog.php"){?> class="active"<?php }?>>Blog</a></li>
+<li><a href="#" <?php if($page_name=="services.php"){?> class="active"<?php }?>>Services</a></li>
+<li><a href="#" <?php if($page_name=="contact.php"){?> class="active"<?php }?>>Contact</a></li>
